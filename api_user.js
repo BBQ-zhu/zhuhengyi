a3c0c09c258e53f94e8bec2cf0db9a47d2acada6
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2020-12-04 17:40:34 +0800

    Update api_user.js

--- api_user.js
+++ api_user.js
@@ -2,9 +2,8 @@
 const Router = require('koa-router')
 const KoaBody = require('koa-body')
 // const { URL, LISTEN } = require('../../config/const.js')
-//如果导出界面的是model.exports={users}则在这里引入时也需要const {user}引入
 const router = new Router()
-const {controlAllUser,controlUser,controlCreateUser} = require('../control/user') //引入control校验
+const {controlAllUser,controlUser,controlCreateUser,controlDelateUser} = require('../control/user') //引入control校验
 
 //查询所有用户
 router.post('/allUser',KoaBody(), async (ctx) => {
@@ -27,6 +26,14 @@ router.post('/user', KoaBody(), async (ctx) => {
 //添加新用户
 router.post('/createUser',KoaBody(), async (ctx)=>{
     let data = await controlCreateUser(ctx.request.body)
+    ctx.body = {
+        data
+    }
+})
+
+//删除用户
+router.post('/delateUser',KoaBody(), async (ctx)=>{
+    let data = await controlDelateUser(ctx.request.body)
     ctx.body = {
         data
     }
