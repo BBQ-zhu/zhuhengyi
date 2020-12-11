56b4f29c5138cf75d6a4aef4b5674cd2f2f6cc37
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2020-12-11 20:23:04 +0800

    Update api_user.js

--- api_user.js
+++ api_user.js
@@ -1,42 +1,20 @@
-//路由层，主要是为前端提供api接口，同时将输入control层的数据返回给前端
-const Router = require('koa-router')
-const KoaBody = require('koa-body')
-// const { URL, LISTEN } = require('../../config/const.js')
-const router = new Router()
-const {controlAllUser,controlUser,controlCreateUser,controlDelateUser} = require('../control/user') //引入control校验
+const { LISTEN } = require('./config/const')
+const Koa = require('koa')
+const Cors = require('koa2-cors')
+const app = new Koa()
 
-//查询所有用户
-router.post('/allUser',KoaBody(), async (ctx) => {
-    //将前端传进来的参数送入conrol层校验
-    let data = await controlAllUser()
-    ctx.body = {
-        data
-    }
+//路由引入
+const user = require('./app/api/user')
+const devices = require('./app/api/devices')
+const task = require('./app/api/task')
+
+app.use(Cors()) //解决跨域问题，位子必须介于路由引入和路由注册之间
+
+//路由注册
+app.use(user.routes(), user.allowedMethods())
+app.use(devices.routes(), devices.allowedMethods())
+app.use(task.routes(), task.allowedMethods())
+
+app.listen(LISTEN, () => {
+    console.log(LISTEN+'端口监听成功！')
 })
-
-//注册用户，查询名称是否已存在
-router.post('/user', KoaBody(), async (ctx) => {
-    //将前端传进来的参数送入conrol层校验
-    let data = await controlUser(ctx.request.body)
-    ctx.body = {
-        data
-    }
-})
-
-//添加新用户
-router.post('/createUser',KoaBody(), async (ctx)=>{
-    let data = await controlCreateUser(ctx.request.body)
-    ctx.body = {
-        data
-    }
-})
-
-//删除用户
-router.post('/delateUser',KoaBody(), async (ctx)=>{
-    let data = await controlDelateUser(ctx.request.body)
-    ctx.body = {
-        data
-    }
-})
-
-module.exports = router
