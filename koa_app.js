9eb26e5916d7413e5cc9eec5a0ec6571c5dc7416
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2020-12-11 20:37:48 +0800

    Update koa_app.js

--- koa_app.js
+++ koa_app.js
@@ -4,15 +4,16 @@ const Cors = require('koa2-cors')
 const app = new Koa()
 
 //路由引入
-const userIndex = require('./app/api/user')
+const user = require('./app/api/user')
+const devices = require('./app/api/devices')
+const task = require('./app/api/task')
 
 app.use(Cors()) //解决跨域问题，位子必须介于路由引入和路由注册之间
 
-
-
-
 //路由注册
-app.use(userIndex.routes(), userIndex.allowedMethods())
+app.use(user.routes(), user.allowedMethods())
+app.use(devices.routes(), devices.allowedMethods())
+app.use(task.routes(), task.allowedMethods())
 
 app.listen(LISTEN, () => {
     console.log(LISTEN+'端口监听成功！')
