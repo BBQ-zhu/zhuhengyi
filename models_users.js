19e7a21f6be4854d80805a132049e38a8a8bae45
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2020-12-11 20:33:00 +0800

    Update models_users.js

--- models_users.js
+++ models_users.js
@@ -3,43 +3,53 @@ module.exports = (sequelize, DataTypes) => {
   let Tasks = sequelize.define('tasks')
   const Users = sequelize.define(
     'users', {
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: DataTypes.INTEGER
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    phone: {
-      type: DataTypes.STRING
-    },
-    hobby: {
-      type: DataTypes.STRING
-    },
-    displed: {
-      type: DataTypes.BOOLEAN
-    },
-    createdAt: {
-      allowNull: false,
-      type: DataTypes.DATE
-    },
-    updatedAt: {
-      allowNull: false,
-      type: DataTypes.DATE
-    }
-  },{
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+        comment:'用户id'
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        comment:'用户名'
+      },
+      password:{
+        type: DataTypes.STRING,
+        allowNull: false,
+        comment:'用户密码'
+      },
+      phone:{
+        type:DataTypes.STRING,
+        comment:'手机号'
+      },
+      hobby:{
+        type:DataTypes.STRING,
+        defalultValue:'无',
+        comment:'爱好，收藏'
+      },
+      displed:{
+        type:DataTypes.BOOLEAN,
+        defalultValue:1,
+        comment:'是否注销用户'
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        comment:'创建时间'
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        comment:'更新时间'
+      }
+    },{
     tableName:'users'
   })
   // Users.associate = function (models) {
     Users.hasMany(Tasks, {
-      foreignKey: 'user_Id'
+      foreignKey: 'user_id'
     });
   // };
   return Users
